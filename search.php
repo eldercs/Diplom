27f7fa40bb00231c6a0471f199091fda5d8068bc
eldercs <deny_1999@ukr.net>
2021-03-26 19:14:47 +0300

    Поиск цены
    Сделал несколько способов искать по цене отеля

--- search.php
+++ search.php
@@ -10,12 +10,15 @@ try {
 }
 $gifs = []; 
 mysqli_query($con,"CREATE FULLTEXT INDEX gif_ft_search ON gifs(title,description)");
-$search = (isset($_GET['search'])) ? trim($_GET['search']) : null;
+$search = (isset($_GET['search'] )) ? trim($_GET['search']) : null;
 $search = mysqli_real_escape_string($con, $search);
 
-/* $search_price = (isset($_GET['price_search'])) ? trim($_GET['price_search']) : null;
-$search_price = mysqli_real_escape_string($con, $search_price); */
-if($search) {
+mysqli_query($con,"CREATE FULLTEXT INDEX gif_ft_search2 ON gifs(price)");
+$search_price = (isset($_GET['price_search'])) ? trim($_GET['price_search']) : null;
+$search_price = mysqli_real_escape_string($con, $search_price);  
+$search_price2 = (isset($_GET['price_search2'])) ? trim($_GET['price_search2']) : null;
+$search_price2 = mysqli_real_escape_string($con, $search_price2);    
+if(!empty($search)) {
     try {
         $hotels = fetchAll($con, "SELECT h.`id`, h.`title` ,`city`, `price`, `count_like`, `image`, `category` AS `category` FROM hotels h JOIN hotel_image ON hotel_image.`id_hotel` = h.`id` JOIN category c ON h.`category_id` = c.`id` WHERE (h.`title` LIKE '%$search%' OR `description` LIKE '%$search%') ORDER BY id DESC ");
     } catch (Exception $e) {
@@ -28,18 +31,10 @@ if($search) {
         'image' => $image,
         'search' => $search
     ]);
-} else {
-    $page_content = shablon('search', [
-        'my_array' => $my_array,
-        'hotels' => null,
-        'image' => null,
-        'search' => $search
-    ]);
 }
-
-/* if($search_price) {
+else if(!empty($search_price) && !empty($search_price2)){
     try {
-        $hotels = fetchAll($con, "SELECT h.`id`, h.`title` ,`city`, `price`, `count_like`, c.`title` AS `category` FROM hotels h JOIN category c ON h.`category_id` = c.`id` WHERE (h.`price` LIKE '%$search%') ORDER BY id DESC");
+        $hotels = fetchAll($con, "SELECT h.`id`, h.`title` ,`city`, `price`, `count_like`, `image`, `category` AS `category` FROM hotels h JOIN hotel_image ON hotel_image.`id_hotel` = h.`id` JOIN category c ON h.`category_id` = c.`id`  WHERE (h.`price` >= $search_price AND h.`price` <= $search_price2) ORDER BY id DESC");
     } catch (Exception $e) {
         renderErrorTemplate($e->getMessage(), $username);
     }
@@ -48,16 +43,67 @@ if($search) {
         'my_array' => $my_array,
         'hotels' => $hotels,
         'image' => $image,
-        'search' => $search
+        'search' => $search_price
     ]);
-} else {
+}
+else if(!empty($search_price)){
+    try {
+        $hotels = fetchAll($con, "SELECT h.`id`, h.`title` ,`city`, `price`, `count_like`, `image`, `category` AS `category` FROM hotels h JOIN hotel_image ON hotel_image.`id_hotel` = h.`id` JOIN category c ON h.`category_id` = c.`id`  WHERE (h.`price` >= $search_price) ORDER BY id DESC");
+    } catch (Exception $e) {
+        renderErrorTemplate($e->getMessage(), $username);
+    }
+    $image = fetchOne($con, "SELECT *  FROM `hotel_image`");
+    $page_content = shablon('search', [
+        'my_array' => $my_array,
+        'hotels' => $hotels,
+        'image' => $image,
+        'search' => $search_price
+    ]);
+}
+else if(!empty($search_price2)){
+    try {
+        $hotels = fetchAll($con, "SELECT h.`id`, h.`title` ,`city`, `price`, `count_like`, `image`, `category` AS `category` FROM hotels h JOIN hotel_image ON hotel_image.`id_hotel` = h.`id` JOIN category c ON h.`category_id` = c.`id`  WHERE (h.`price` <= $search_price2) ORDER BY id DESC");
+    } catch (Exception $e) {
+        renderErrorTemplate($e->getMessage(), $username);
+    }
+    $image = fetchOne($con, "SELECT *  FROM `hotel_image`");
+    $page_content = shablon('search', [
+        'my_array' => $my_array,
+        'hotels' => $hotels,
+        'image' => $image,
+        'search' => $search_price
+    ]);
+}
+else {
     $page_content = shablon('search', [
         'my_array' => $my_array,
         'hotels' => null,
         'image' => null,
         'search' => $search
     ]);
-} */
+}
+
+/* if(!empty($search_price)) {
+    try {
+        $hotels = fetchAll($con, "SELECT h.`id`, h.`title` ,`city`, `price`, `count_like`, `image`, `category` AS `category` FROM hotels h JOIN hotel_image ON hotel_image.`id_hotel` = h.`id` JOIN category c ON h.`category_id` = c.`id`  WHERE (h.`price` LIKE '%$search_price%') ORDER BY id DESC");
+    } catch (Exception $e) {
+        renderErrorTemplate($e->getMessage(), $username);
+    }
+    $image = fetchOne($con, "SELECT *  FROM `hotel_image`");
+    $page_content = shablon('search', [
+        'my_array' => $my_array,
+        'hotels' => $hotels,
+        'image' => $image,
+        'search' => $search_price
+    ]);
+} else {
+    $page_content = shablon('search', [
+        'my_array' => $my_array,
+        'hotels' => null,
+        'image' => null,
+        'search' => $search_price
+    ]);
+}    */
 
 echo shablon(
     'layout',
